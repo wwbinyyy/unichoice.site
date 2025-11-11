74ef90a2e02e7bb18485adc14c329019b0651db9
wwbinyyy <160769228+wwbinyyy@users.noreply.github.com>
2025-11-11 23:11:58 +0500

    Add Home component for the Add Home page component to fix 404home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1>Welcome to UniChoice</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
